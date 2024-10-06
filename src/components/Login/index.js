2b953a3629d734aa6be3926d2e91c32a1d290ed1
Medurivijay07 <medurivijaykumar07@gmail.com>
2024-10-06 12:26:14 +0000

    login page

---
+++ src/components/Login/index.js
@@ -0,0 +1,106 @@
+import {Component} from 'react'
+
+import Cookies from 'js-cookie'
+
+import './index.css'
+
+class Login extends Component {
+  state = {username: '', password: '', errorMsg: '', showErrMsg: false}
+
+  onChangeUsername = event => {
+    this.setState({username: event.target.value})
+  }
+
+  onChangePassword = event => {
+    this.setState({password: event.target.value})
+  }
+
+  onSubmitSuccess = jwtToken => {
+    Cookies.set('jwt_token', jwtToken, {expires: 30})
+  }
+
+  onSubmitFailure = errorMsg => {
+    this.setState({showErrMsg: true, errorMsg})
+  }
+
+  onSubmitForm = async event => {
+    event.preventDefault()
+    const {username, password} = this.state
+    const userDetails = {username, password}
+    const apiUrl = 'https://apis.ccbp.in/login'
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(userDetails),
+    }
+    const response = await fetch(apiUrl, options)
+    const data = await response.json()
+    if (response.ok) {
+      this.onSubmitSuccess(data.jwt_token)
+    } else {
+      this.onSubmitFailure(data.error_msg)
+    }
+  }
+
+  renderUsername = () => {
+    const {username} = this.state
+
+    return (
+      <>
+        <label className="label-element" htmlFor="username">
+          USERNAME
+        </label>
+        <input
+          type="text"
+          className="input-element"
+          id="username"
+          value={username}
+          onChange={this.onChangeUsername}
+        />
+      </>
+    )
+  }
+
+  renderPassword = () => {
+    const {password} = this.state
+
+    return (
+      <>
+        <label className="label-element" htmlFor="password">
+          PASSWORD
+        </label>
+        <input
+          type="password"
+          className="input-element"
+          id="password"
+          value={password}
+          onChange={this.onChangePassword}
+        />
+      </>
+    )
+  }
+
+  render() {
+    const {showErrMsg, errorMsg} = this.state
+
+    return (
+      <div className="login-main-container">
+        <h1 className="movies-title">MOVIES</h1>
+        <div className="responsive-login-container">
+          <div className="login-box">
+            <h1 className="login-title">Login</h1>
+            <form className="form-container" onSubmit={this.onSubmitForm}>
+              {this.renderUsername()}
+              {this.renderPassword()}
+              {showErrMsg && <p className="error-message">{errorMsg}</p>}
+              <button type="submit" className="login-button">
+                Login
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Login
